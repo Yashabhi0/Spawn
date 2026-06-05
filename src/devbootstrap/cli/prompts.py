7280dad9380281e354b0890f21dd416b03817d25
Abhiix0 <24r21a6778@mlrit.ac.in>
2026-06-05 15:08:03 +0530

    Polish: Provided template choices for better ux

--- src/devbootstrap/cli/prompts.py
+++ src/devbootstrap/cli/prompts.py
@@ -5,23 +5,23 @@ from devbootstrap.core.models import ProjectConfig
 
 def get_project_config() -> ProjectConfig:
     project_name = typer.prompt("Project Name")
-    VALID_TEMPLATES = [
-    "python",
-    "fastapi",
-    "data-science",
-    "ml",
-    ]
 
-    template = typer.prompt(
-        "Template (python, fastapi, data-science, ml)"
-    )
+    TEMPLATE_CHOICES = {
+    "1": "python",
+    "2": "fastapi",
+    "3": "data-science",
+    "4": "ml",
+    }
+    menu = "\n".join([f" [{k}] {v}" for k, v in TEMPLATE_CHOICES.items()])
+    print(f"\nAvailable Templates:\n{menu}\n")
 
-    while template not in VALID_TEMPLATES:
-      print("Invalid template.")
-    
-      template = typer.prompt(
-        "Template (python, fastapi, data-science, ml)"
-        )
+    choice = typer.prompt("Choose template")
+
+    while choice not in TEMPLATE_CHOICES:
+        typer.secho(" Invalid choice. Please select a valid number.", fg=typer.colors.RED)
+        choice = typer.prompt("Choose template")
+
+    template = TEMPLATE_CHOICES[choice]
 
     use_git = typer.confirm(
         "Initialize Git?",
